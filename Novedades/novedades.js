108a5d848d5e6b5b027ae96ded943fcceaa9dc23
adrianrc232 <adrian23erick@gmail.com>
2025-10-02 23:35:57 -0500

    actualizaciones y novedades

---
+++ Novedades/novedades.js
@@ -0,0 +1,24 @@
+const feedUrl = 'http://feeds.twit.tv/brickhouse.xml';  // URL del RSS feed
+
+fetch(`https://api.rss2json.com/v1/api.json?rss_url=${encodeURIComponent(feedUrl)}`)
+  .then(response => response.json())
+  .then(data => {
+    const noticiasList = document.getElementById('noticias-list');
+    noticiasList.innerHTML = '';  // Limpiar las noticias previas
+
+    // Mostrar las noticias
+    data.items.forEach(item => {
+      const li = document.createElement('li');
+      
+      li.innerHTML = `
+        <div class="noticia-item">
+            <strong>${item.title}</strong>
+            <p>${item.description}</p>
+            <a href="${item.link}" target="_blank">Leer más</a>
+        </div>
+      `;
+      
+      noticiasList.appendChild(li);
+    });
+  })
+  .catch(error => console.error('Error al cargar el feed RSS:', error));
